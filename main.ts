acb884772de6a952f7e0580020d0b5dd096d0d09
raelum <100334527+raelum@users.noreply.github.com>
2024-08-05 21:07:42 -0700

    Add support for completing/progressing tasks while preserving hieracrchical context

--- main.ts
+++ main.ts
@@ -1,134 +1,329 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+// How to develop
+// - Navigate to .plugins/obsidian-brain folder
+// - npm run dev
+// - Change code and it should rebuild, triggering hot reload on Obsidian
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+import { Editor, EditorChange, EditorPosition, Plugin } from 'obsidian';
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  async onload() {
+    this.addCommand({
+      id: 'complete-task',
+      name: 'Complete Task',
+      editorCallback: (editor: Editor) => {
+        archiveTask(editor, true);
+      }
+    });
 
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    this.addCommand({
+      id: 'progress-task',
+      name: 'Progress Task',
+      editorCallback: (editor: Editor) => {
+        archiveTask(editor, false);
+      }
+    });
+  }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class Task {
+  parents: string[];
+  task: string;
+  children: string[];
+  // Regex that matches a bullet task syntax "- [ ] " while capturing
+  // the leading whitespaces as a group to insert into the final output.
+  // (\s*) is capturing the white spaces, and $1 uses the captured value.
+  bulletTaskRegex: RegExp = /^(\s*)- \[ \]/;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  constructor(parents: string[], task: string, children: string[]) {
+    // Convert parent tasks to regular bullet points
+    this.parents = parents.map((p) => { return p.replace(this.bulletTaskRegex, "$1-") });
+    this.task = task;
+    this.children = children;
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+  // Mark the task as completed.
+  markAsCompleted(): void {
+    this.task = this.task.replace(this.bulletTaskRegex, "$1- [x]");
+  }
+
+  // Mark the task as in progress.
+  markAsInProgress(): void {
+    this.task = this.task.replace(this.bulletTaskRegex, "$1- [/]");
+  }
+
+  toString(includeParentIndex: number): string {
+    var includedParents = this.parents.slice(includeParentIndex);
+    if (includedParents.length > 0) {
+      return includedParents.join("\n") + "\n" + this.task;
+    } else {
+      return this.task;
+    }
+  }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class Markdown {
+  editor: Editor;
+  changes: EditorChange[];
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(editor: Editor) {
+    this.editor = editor;
+    this.changes = [];
+  }
 
-	display(): void {
-		const {containerEl} = this;
+  // Checks if the given line is a task.
+  isBullet(lineNumber: number): boolean {
+    // TODO: Replace this with regex match
+    return lineNumber <= this.editor.lastLine() && this.editor.getLine(lineNumber).contains("- ");
+  }
 
-		containerEl.empty();
+  // Checks if the given line is a task.
+  isBulletTask(lineNumber: number): boolean {
+    // TODO: Replace this with regex match
+    return lineNumber <= this.editor.lastLine() &&
+      (this.editor.getLine(lineNumber).contains("- [ ] ") || this.editor.getLine(lineNumber).contains("- [x] "));
+  }
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  isArchiveTask(lineNumber: number): boolean {
+    return this.isBullet(lineNumber) || this.isBulletTask(lineNumber);
+  }
+
+  // Returns the indent level of the given line.
+  indentLevel(lineNumber: number): number {
+    const match = this.editor.getLine(lineNumber).match(/^\t*/);
+    return match ? match[0].length : 0;
+  }
+
+  // Gets a Task object for the given line.
+  getTask(lineNumber: number): Task | null {
+    if (!this.isBulletTask(lineNumber)) {
+      return null;
+    }
+
+    // Get the current task.
+    var task: string = this.editor.getLine(lineNumber);
+    var taskIndentLevel: number = this.indentLevel(lineNumber);
+
+    // Get the current task's parents.
+    var parents: string[] = [];
+    var parentLineNumber = lineNumber - 1;
+    var currentIndentLevel = taskIndentLevel;
+    for (var parentIndentLevel = taskIndentLevel - 1; parentIndentLevel >= 0; parentIndentLevel--) {
+      // Iterate once for every parent we want to find.
+      while (parentLineNumber >= 0 && this.indentLevel(parentLineNumber) >= currentIndentLevel) {
+        // Keep iterating until we find a line that has a lower indent level than the current task.
+        parentLineNumber--;
+      }
+      if (parentLineNumber >= 0 &&
+        this.isBulletTask(parentLineNumber) &&
+        this.indentLevel(parentLineNumber) == parentIndentLevel) {
+        currentIndentLevel = parentIndentLevel;
+        // Since we're iterating over parents backwards, add them to the front of the list.
+        parents.unshift(this.editor.getLine(parentLineNumber));
+      } else {
+        // Either we reached the beginning of the file, the current line is a task, or we saw an unexpected indent level
+        // indicating a malformed task list. Abort finding parents.
+        break;
+      }
+    }
+
+    var children: string[] = [];
+
+    return new Task(parents, task, children);
+  }
+
+  appendAfterLine(lineNumber: number, text: string) {
+    this.changes.push({ text: "\n" + text, from: this.lineEndPosition(lineNumber) });
+  }
+
+  // Appends the given text to the end of the file starting from a new line.
+  appendToEnd(text: string): void {
+    this.appendAfterLine(this.editor.lastLine(), text);
+  }
+
+  replaceLine(lineNumber: number, text: string) {
+    this.changes.push({ text: text, from: this.lineStartPosition(lineNumber), to: this.lineEndPosition(lineNumber) });
+  }
+
+  // Delete the given line range. If a range isn't given, it defaults to deleting one line.
+  deleteLine(fromLine: number, toLine?: number) {
+    var startPosition: EditorPosition;
+    if (fromLine == 0) {
+      startPosition = this.lineStartPosition(0);
+    } else {
+      startPosition = this.lineEndPosition(fromLine - 1);
+    }
+
+    var endPosition: EditorPosition;
+    if (toLine == undefined) {
+      endPosition = this.lineEndPosition(fromLine);
+    } else {
+      endPosition = this.lineEndPosition(toLine);
+    }
+
+    this.changes.push({ text: "", from: startPosition, to: endPosition });
+  }
+
+  // Apply all changes as 1 transaction so that command + z undos all of them together.
+  applyChanges(): void {
+    this.editor.transaction({ changes: this.changes });
+  }
+
+  private lineStartPosition(lineNumber: number): EditorPosition {
+    return { line: lineNumber, ch: 0 };
+  }
+
+  private lineEndPosition(lineNumber: number): EditorPosition {
+    return { line: lineNumber, ch: this.editor.getLine(lineNumber).length };
+  }
+}
+
+// Gets today's date in YYYY-MM-DD format.
+function getLocalDateString(): string {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const day = String(now.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
+
+// Removes any surrounding whitespace or preceding bullet/task markings so we can
+// just see the task description.
+function stripTask(task: string): string {
+  // Remove any surrounding whitespaces since tasks can be indented.
+  task = task.trim();
+
+  // Define the preceding patterns to be removed.
+  const patterns = ["- [ ] ", "- [x] ", "- "];
+
+  // Remove any pattern found at the beginning of the string.
+  for (const pattern of patterns) {
+    if (task.startsWith(pattern)) {
+      return task.slice(pattern.length);
+    }
+  }
+  return task;
+}
+
+// Compares 2 tasks by first removing any preceding checkbox, bullet points, or spaces.
+function isSameTask(task1: string, task2: string): boolean {
+  return stripTask(task1) == stripTask(task2);
+}
+
+function archiveTask(editor: Editor, completeTask: boolean): void {
+  // Get current line information.
+  var currentPosition: EditorPosition = editor.getCursor();
+  var taskLineNumber: number = currentPosition.line;
+  var md: Markdown = new Markdown(editor);
+
+  // Don't do anything if current line isn't a task.
+  if (!md.isBulletTask(taskLineNumber)) {
+    return;
+  }
+
+  var taskResult: Task | null = md.getTask(taskLineNumber);
+  if (taskResult == null) {
+    return;
+  }
+  var task: Task = taskResult as Task;
+  if (completeTask) {
+    task.markAsCompleted();
+  } else {
+    task.markAsInProgress();
+  }
+
+  // Find history section.
+  var historyLineNumber: number = -1;
+  for (var i = taskLineNumber + 1; i < editor.lineCount(); i++) {
+    var line = editor.getLine(i);
+    if (line == "# History") {
+      historyLineNumber = i;
+      break;
+    }
+  }
+
+  // Create history section if it doesn't exist.
+  if (historyLineNumber == -1) {
+    md.appendToEnd("# History");
+    historyLineNumber = editor.lineCount();
+  }
+
+  // Find today section.
+  let today = getLocalDateString();
+  var todayLineNumber = -1;
+  for (var i = historyLineNumber + 1; i < editor.lineCount(); i++) {
+    var line = editor.getLine(i);
+    if (line == "## " + today) {
+      todayLineNumber = i;
+      break;
+    }
+  }
+
+  // Create today subsection if it doesn't exist.
+  var createdTodaySection = false;
+  if (todayLineNumber == -1) {
+    md.appendAfterLine(historyLineNumber, "## " + today);
+    // We appended the section but it doesn't truly exist until changes are applied. For all intents and purposes, the
+    // true line number for today section is same as history section since it's being inserted at that spot.
+    todayLineNumber = historyLineNumber;
+    createdTodaySection = true;
+  }
+
+  if (createdTodaySection) {
+    // Today section was just created, so we can insert the task without performing a search.
+    md.appendAfterLine(todayLineNumber, task.toString(0));
+  } else {
+    // Today section already exists, so search for 
+    var archiveTaskLineNumber = todayLineNumber + 1;
+    var currentParentLevel = 0;
+    for (; currentParentLevel < task.parents.length; currentParentLevel++) {
+      while (md.isArchiveTask(archiveTaskLineNumber) &&
+        // Either the current task is a subtask compared to the parent.
+        (md.indentLevel(archiveTaskLineNumber) > currentParentLevel ||
+          // Or the current task is a sibling of the parent, but not a match yet.
+          (md.indentLevel(archiveTaskLineNumber) == currentParentLevel &&
+            !isSameTask(editor.getLine(archiveTaskLineNumber), task.parents[currentParentLevel])))) {
+        archiveTaskLineNumber++;
+      }
+      if (!md.isArchiveTask(archiveTaskLineNumber) || md.indentLevel(archiveTaskLineNumber) < currentParentLevel) {
+        // We reached the end of the task (or subtask) list.
+        break;
+      } else if (md.indentLevel(archiveTaskLineNumber) == currentParentLevel &&
+        isSameTask(editor.getLine(archiveTaskLineNumber), task.parents[currentParentLevel])) {
+        // We reached the matching parent task.
+        archiveTaskLineNumber++;
+
+      }
+    }
+
+    // Iterate to the end of the current list (or sublist).
+    while (md.isArchiveTask(archiveTaskLineNumber) && md.indentLevel(archiveTaskLineNumber) >= currentParentLevel) {
+      // Exit early if we manage to find the current task.
+      if (md.indentLevel(archiveTaskLineNumber) == currentParentLevel &&
+        isSameTask(editor.getLine(archiveTaskLineNumber), task.task)) {
+        break;
+      }
+      archiveTaskLineNumber++;
+    }
+
+    if (md.indentLevel(archiveTaskLineNumber) == currentParentLevel &&
+      isSameTask(editor.getLine(archiveTaskLineNumber), task.task)) {
+      // Replace existing bullet task to completed one.
+      md.replaceLine(archiveTaskLineNumber, task.task);
+    } else {
+      // Add task to list under section.
+      md.appendAfterLine(archiveTaskLineNumber - 1, task.toString(currentParentLevel));
+    }
+  }
+
+  // Delete task only if the user is marking the task as fully completed.
+  // NOTE: We do this at the end since deleted lines impacts line numbers of other changes.
+  if (completeTask) {
+    md.deleteLine(taskLineNumber);
+  }
+
+  // Apply all changes as 1 transaction so that command + z undos all of them together.
+  md.applyChanges();
+
+  // Reset the cursor back to the original position.
+  editor.setCursor(currentPosition);
 }
